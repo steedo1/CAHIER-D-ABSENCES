2f270d6ea8990382b7bc4eb152df85e17e664e25
Ange Aristide <ariskouadio22@gmail.com>
2025-12-12 05:15:45 +0000

    BUG NOTE 2 MATIERES

--- src/app/api/admin/teachers/subjects/add/route.ts
+++ src/app/api/admin/teachers/subjects/add/route.ts
@@ -1,3 +1,4 @@
+//src/app/api/admin/teachers/subjects/add/route.ts
 import { NextRequest, NextResponse } from "next/server";
 import { getSupabaseServerClient } from "@/lib/supabase-server";
 import { getSupabaseServiceClient } from "@/lib/supabaseAdmin";
